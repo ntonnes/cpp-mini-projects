4a07f378c5b71effde0f402bd9fa1a9be89638b3
Noah Tonnesen <noah.tonnesen@gmail.com>
2024-03-21 17:07:11 -0400

    stuck

--- health_assistant_v3/assignment3.cpp
+++ health_assistant_v3/assignment3.cpp
@@ -30,6 +30,7 @@ class UserInfoManager
             std::string lifestyle;
         };
         std::vector<UserInfo> mylist;
+        
 
         // Template representing inputs for validateInput function
         // The first type is the attribute type (int, double, or string)
@@ -72,12 +73,28 @@ class UserInfoManager
             }
         }
 
+        // Finds a user in the list
+        UserInfo& findUser(const std::string& username) {
+            // Find user according to username
+            auto user = std::find_if(mylist.begin(), mylist.end(), [&username](const UserInfo& user) {
+                return user.name == username;
+            });
+            // Throw an error if user not found
+            if (user == mylist.end()) {
+                throw std::runtime_error("User with name " + username + " does not exist.");
+            }
+            // Return user if found
+            return *user;
+        }
+
     public:
         // Constructor
-        UserInfoManager() {}
+        UserInfoManager() : mylist() {}
 
         // Destructor
         ~UserInfoManager() {}
+
+        std::vector<UserInfo> getMylist() { return mylist; }
 
         // Adds a user and their input information to the list
         void addUserInfo() {
@@ -95,7 +112,7 @@ class UserInfoManager
                 }
             }
             newUser.name = input;
-            
+
             // Prompt for and validate gender
             std::vector<std::string> validGenders = { "female", "male" };
             validateInput(
@@ -112,7 +129,7 @@ class UserInfoManager
                 ageRange, newUser.age
             );
 
-            // Prompt for and validate double-based measurements
+            // create a vector of pairs to simplify the measurement input validation process
             std::vector<std::pair<std::string, double*>> attributes = {
                 {"Enter your body weight in kilograms: ", &newUser.weight},
                 {"Enter your waist measurement in centimeters: ", &newUser.waist},
@@ -121,23 +138,26 @@ class UserInfoManager
                 {"Enter your hip measurement in centimeters: ", &newUser.hip}
             };
 
-            // Prompt for and validate hip measurement if user is female
+            // Prompt for and validate body measurements
+            std::pair<double, double> measurementRange = { 0, std::numeric_limits<double>::max() };
             for (auto& attribute : attributes) {
+                // Skip hip measurement if the user is a male
                 if ((attribute.second == &newUser.hip) && (newUser.gender == "male")){
                     continue;
                 };
-                validateDouble(
+                validateInput(
                     attribute.first, 
-                    "Invalid measurement. Please enter a value greater than 0: ", 
-                    0, std::numeric_limits<double>::max(), *attribute.second
+                    "Invalid measurement. Measurement must be greater than 0.0.", 
+                    measurementRange, *attribute.second
                 );
             };  
 
             // Prompt for and validate lifestyle
-            validateString(
+            std::vector<std::string> validLifestyles = { "sedentary", "moderate", "active" };
+            validateInput(
                 "Enter information about your current lifestyle (sedentary, moderate, or active): ", 
-                "Invalid lifestyle. Please enter sedentary, moderate, or active: ", 
-                { "sedentary", "moderate", "active" }, newUser.lifestyle
+                "Invalid lifestyle. Lifestyle must be either sedentary, moderate, or active.", 
+                validLifestyles, newUser.lifestyle
             );
 
             // Print success message and add user to list
@@ -160,18 +180,33 @@ class UserInfoManager
             std::cout << "User " << username << " has been deleted." << std::endl;
         }
 
-        UserInfo findUser(const std::string& username) {
-            // Find user according to username
-            auto user = std::find_if(mylist.begin(), mylist.end(), [&username](const UserInfo& user) {
-                return user.name == username;
-            });
-            // Throw an error if user not found
-            if (user == mylist.end()) {
-                throw std::runtime_error("User with name " + username + " does not exist.");
+        // Getter methods
+        std::vector<std::string> getAllUsernames() {
+            std::vector<std::string> usernames;
+            for (const UserInfo& user : mylist) {
+                usernames.push_back(user.name);
             }
-            // Return user if found
-            return *(user);
+            return usernames;
         }
+        int getAge(const std::string& username) { return findUser(username).age; }
+        std::string getGender(const std::string& username) { return findUser(username).gender; }
+        double getWeight(const std::string& username) { return findUser(username).weight; }
+        double getWaist(const std::string& username) { return findUser(username).waist; }
+        double getNeck(const std::string& username) { return findUser(username).neck; }
+        double getHeight(const std::string& username) { return findUser(username).height; }
+        double getHip(const std::string& username) { return findUser(username).hip; }
+        std::pair<int, std::string> getBfp(const std::string& username) { return findUser(username).bfp; }
+        double getCalories(const std::string& username) { return findUser(username).calories; }
+        std::string getLifestyle(const std::string& username) { return findUser(username).lifestyle; }
+
+        // Setter methods
+        void setBfp(const std::string& username, std::pair<int, std::string> bfp) { findUser(username).bfp = bfp; }
+        void setCalories(const std::string& username, double calories) { findUser(username).calories = calories; }
+        void setCarbs(const std::string& username, double carbs) { findUser(username).carbs = carbs; }
+        void setProtein(const std::string& username, double protein) { findUser(username).protein = protein; }
+        void setFat(const std::string& username, double fat) { findUser(username).fat = fat; }
+        void setLifestyle(const std::string& username, std::string lifestyle) { findUser(username).lifestyle = lifestyle; }
+
 
         // Reads and populates list from file
         void readFromFile(std::string filename) {
@@ -263,7 +298,7 @@ class UserInfoManager
             std::cout << "\033[1;33m|\033[0m" << "\033[1;36m  Neck:\033[0m                 " << user.neck << " cm\n";
             std::cout << "\033[1;33m|\033[0m" << "\033[1;36m  Height:\033[0m               " << user.height << " cm\n";
             if (user.gender == "female") {
-                std::cout << "\033[1;33m|\033[0m" << "\033[1;36m Hips:\033[0m              " << user.hip << " cm\n";
+                std::cout << "\033[1;33m|\033[0m" << "\033[1;36m  Hips:\033[0m              " << user.hip << " cm\n";
             }
             std::cout << "\033[1;33m|\033[0m" << "\033[1;36m  Lifestyle:\033[0m            " << user.lifestyle << "\n";
             std::cout << "\033[1;33m|\033[0m\n";
@@ -275,43 +310,289 @@ class UserInfoManager
             std::cout << "\033[1;33m|\033[0m" << "\033[1;35m  Fat:\033[0m                  " << user.fat << " grams\n";
             std::cout << "\033[1;33m=========================================\033[0m\n\n";
         }
-    
 };
 
 class HealthAssistant {
-    private:
-        UserInfoManager mymanager;
+    protected:
+        static UserInfoManager mymanager;
 
     public:
-        virtual double getBfp(std::string username) = 0;
+        virtual void getBfp(std::string username) = 0;
+
+        // Method to get user details
+        void getUserDetail() { mymanager.addUserInfo(); }
+        std::vector<std::string> getAllUsernames() { return mymanager.getAllUsernames(); }
+
+        // Method to get the body fat percentage group for a user
+        std::string getGroup(double bfp, int age, std::string gender) {
+            std::vector<std::pair<int, std::string>> ranges;
+            if (gender == "female") {
+                if (age >= 20 && age <= 39) ranges = {{21, "low"}, {33, "normal"}, {39, "high"}};
+                else if (age >= 40 && age <= 59) ranges = {{23, "low"}, {34, "normal"}, {40, "high"}};
+                else if (age >= 60 && age <= 79) ranges = {{24, "low"}, {36, "normal"}, {42, "high"}};
+            } else if (gender == "male") {
+                if (age >= 20 && age <= 39) ranges = {{8, "low"}, {20, "normal"}, {25, "high"}};
+                else if (age >= 40 && age <= 59) ranges = {{11, "low"}, {22, "normal"}, {28, "high"}};
+                else if (age >= 60) ranges = {{13, "low"}, {25, "normal"}, {30, "high"}};
+            }
+
+            for (auto& range : ranges) {
+                if (bfp < range.first) return range.second;
+            }
+            return "very high";
+        }
 
         // Method to get the recommended daily calorie intake for a user
         void getDailyCalories(std::string username){
             // Get user information from the linked list
-            mymanager.findUser(username).calories = 1600;
+            int age = mymanager.getAge(username);
+            std::string gender = mymanager.getGender(username);
+            std::string lifestyle = mymanager.getLifestyle(username);
 
             // Set base calorie intake
             int calories = 1600;
 
             // Add additional calories based on age
-            if (user->age < 51) { 
-                calories += (user->age > 30) ? 200 : 400;
+            if (age < 51) { 
+                calories += (age > 30) ? 200 : 400;
             }
 
             // Add additional calories for males
-            calories += (user->gender == "male") ? 400 : 0;
+            calories += (gender == "male") ? 400 : 0;
 
             // Set scaling factor for activity level
-            int activityBonus = (user->gender == "male") ? 300 : 200;
+            int activityBonus = (gender == "male") ? 300 : 200;
 
             // Add additional calories based on activity level
-            if (user->lifestyle != "sedentary") {
-                calories += (user->lifestyle == "moderate") ? activityBonus : (2*activityBonus);
+            if (lifestyle != "sedentary") {
+                calories += (lifestyle == "moderate") ? activityBonus : (2*activityBonus);
             }
 
             // Set the user's daily calorie intake in the linked list and print the result
-            user->calories = calories;
+            mymanager.setCalories(username, calories);
             std::cout << "\nRecommended daily calorie intake for " << username << " is " << calories << " calories." << std::endl;
         }
-    
+
+        // Method to get the recommended macronutrient breakdown for a user
+        void getMealPrep(std::string username){
+            // Constants for macronutrient calorie values
+            const int carb_calories = 4;
+            const int protein_calories = 4;
+            const int fat_calories = 9;
+
+            // Constants for recommended macronutrient breakdown percentages
+            const double carb_percent = 0.5;
+            const double protein_percent = 0.3;
+            const double fat_percent = 0.2;
+
+            // If the user's daily calorie intake has not been calculated, print an error and return
+            int calories = mymanager.getCalories(username);
+            if (calories == 0) {
+                throw std::runtime_error("A user's daily calorie intake must be calculated before their meal prep.");
+            }
+
+            // Calculate grams for each macronutrient and set the user's macronutrient breakdown
+            mymanager.setCarbs(username, (calories * carb_percent) / carb_calories);
+            mymanager.setProtein(username, (calories * protein_percent) / protein_calories);
+            mymanager.setFat(username, (calories * fat_percent) / fat_calories);
+        }
+
+        // Method to calculate body fat percentage, daily calorie intake, and macronutrient breakdown for all users in the linked list
+        void massLoadAndCompute(std::string filename){
+            // Read user information from file
+            mymanager.readFromFile(filename);
+            // Iterate through the linked list and calculate body fat percentage, daily calorie intake, and macronutrient breakdown for each user
+            for (std::string username : getAllUsernames()) {
+                getBfp(username);
+                getDailyCalories(username);
+                getMealPrep(username);
+            }
+        }
+
+        // Wrapper method to display user information
+        void display(std::string username){ mymanager.display(username); }; 
+
+        // Wrapper method to serialize user information
+        void serialize(std::string filename){ mymanager.writeToFile(filename); }; 
+
+        // Wrapper method to read user information from file
+        void readFromFile(std::string filename){ mymanager.readFromFile(filename);}; 
+
+        // Wrapper method to delete user
+        void deleteUser(std::string username){ mymanager.deleteUser(username);}; 
 };
+
+class USNavyMethod : public HealthAssistant {
+    public:
+        void getBfp(std::string username) {
+            double bfp;
+            std::string gender = mymanager.getGender(username);
+            int age = mymanager.getAge(username);
+            double waist = mymanager.getWaist(username);
+            double neck = mymanager.getNeck(username);
+            double hip = mymanager.getHip(username);
+            double height = mymanager.getHeight(username);
+
+            if (gender == "male") {
+                bfp = 495 / (1.0324 - 0.19077 * log10(waist - neck) + 0.15456 * log10(height)) - 450;
+            } else {
+                bfp = 495 / (1.29579 - 0.35004 * log10(waist + hip - neck) + 0.22100 * log10(height)) - 450;
+            }
+
+            std::string group = getGroup(bfp, age, gender);
+            mymanager.setBfp(username, {bfp, group});
+            std::cout << "\nBody fat percentage for " << username << " is " << bfp << "%, which is " << group << "." << std::endl;
+        }
+};
+
+class BmiMethod : public HealthAssistant {
+    public:
+        void getBfp (std::string username){
+            double weight = mymanager.getWeight(username);
+            double divisor = std::pow(mymanager.getHeight(username)/ 100, 2);
+            double bfp = (weight / divisor);
+            std::string group = getGroup(bfp, mymanager.getAge(username), mymanager.getGender(username));
+            mymanager.setBfp(username, {bfp, group});
+        }
+};
+
+class UserStats {
+private:
+    UserInfoManager statmanager;
+
+public:
+
+    std::vector<std::string> GetHealthyUsers(std::string method, std::string gender = "") {
+
+        std::vector<std::string> healthyUsers;
+        HealthAssistant* ha;
+
+        if (method == "USNavy"){
+            ha = new USNavyMethod();
+        } else if (method == "bmi"){
+            ha = new BmiMethod();
+        } else if (method == "all") {
+            std::vector<std::string> healthyUsersNavy = GetHealthyUsers("USNavy", gender);
+            std::vector<std::string> healthyUsersBmi = GetHealthyUsers("bmi", gender);
+            healthyUsers.insert(healthyUsers.end(), healthyUsersNavy.begin(), healthyUsersNavy.end());
+            healthyUsers.insert(healthyUsers.end(), healthyUsersBmi.begin(), healthyUsersBmi.end());
+            return healthyUsers;
+        } else {
+            // Handle invalid method
+            throw std::invalid_argument("Invalid method. Please enter USNavy, bmi, or all.");
+        }
+
+        ha->massLoadAndCompute("user_data.csv");
+        for (std::string username : ha->getAllUsernames()){
+            if (statmanager.getBfp(username).second == "normal") {
+                if (gender.empty() || statmanager.getGender(username) == gender) {
+                    healthyUsers.push_back(username);
+                }
+            }
+        }
+        delete ha;
+        return healthyUsers;
+    }
+
+    std::vector<std::string> GetUnfitUsers(std::string method, std::string gender = "") {
+        std::vector<std::string> unfitUsers;
+        HealthAssistant* ha;
+
+        if (method == "USNavy"){
+            ha = new USNavyMethod();
+        } else if (method == "bmi"){
+            ha = new BmiMethod();
+        } else if (method == "all") {
+            std::vector<std::string> unfitUsersNavy = GetUnfitUsers("USNavy", gender);
+            std::vector<std::string> unfitUsersBmi = GetUnfitUsers("bmi", gender);
+            unfitUsers.insert(unfitUsers.end(), unfitUsersNavy.begin(), unfitUsersNavy.end());
+            unfitUsers.insert(unfitUsers.end(), unfitUsersBmi.begin(), unfitUsersBmi.end());
+            return unfitUsers;
+        } else {
+            // Handle invalid method
+            throw std::invalid_argument("Invalid method. Please enter USNavy, bmi, or all.");
+        }
+
+        ha->massLoadAndCompute("user_data.csv");
+        std::vector<std::string> usernames = ha->getAllUsernames();
+        for (std::string username : usernames){
+            std::string bfpStatus = mymanager.getBfp(username).second;
+            if (bfpStatus == "high" || bfpStatus == "very high") {
+                if (gender.empty() || mymanager.getGender(username) == gender) {
+                    unfitUsers.push_back(username);
+                }
+            }
+        }
+        delete ha;
+        return unfitUsers;
+    }
+
+    void GetFullStats() {
+        mymanager.massLoadAndCompute("user_data.csv");
+        std::vector<std::string> usernames = mymanager.getAllUsernames();
+        for (std::string username : usernames){
+            std::cout << "User: " << username << std::endl;
+            mymanager.display(username);
+        }
+    }
+};
+
+UserInfoManager HealthAssistant::mymanager = UserInfoManager();
+
+// Main function
+int main() {
+    HealthAssistant* ha = new USNavyMethod();
+    std::string userInput;
+    // Loop until the user inputs "exit"
+    while (true) {
+        // Get user details
+        ha->getUserDetail();
+        // Ask for user input
+        std::cout << "Enter 'exit' to quit, or press Enter to continue:";
+        std::getline(std::cin, userInput);
+        // Check if the user wants to exit
+        if (userInput == "exit") {
+            break; // Exit the loop
+        }
+    }
+    ha->display("all");
+    ha->getBfp("john");
+    ha->getDailyCalories("john");
+    ha->getMealPrep("john");
+    ha->serialize("us_user_data->csv");
+    delete ha;
+    ha = new BmiMethod();
+    // Loop until the user inputs "exit"
+    while (true) {
+        // Get user details
+        ha->getUserDetail();
+        // Ask for user input
+        std::cout << "Enter 'exit' to quit, or press Enter to continue:";
+        std::getline(std::cin, userInput);
+        // Check if the user wants to exit
+        if (userInput == "exit") {
+        break; // Exit the loop
+        }
+    }
+    ha->display("all");
+    ha->getBfp("john");
+    ha->getDailyCalories("john");
+    ha->getMealPrep("john");
+    ha->deleteUser("jack"); // assuming we have a user jack
+    ha->display("all"); // should display all except for jack
+    ha->serialize("bmi_user_data->csv");
+    delete ha;
+    ha = new USNavyMethod();
+    ha->massLoadAndCompute("us_user_data.csv");
+    ha->display("all");
+    delete ha;
+    ha = new BmiMethod();
+    ha->massLoadAndCompute("bmi_user_data.csv");
+    ha->display("all");
+    delete ha;
+    UserStats stat;
+    stat.GetHealthyUsers("bmi", "female");
+    stat.GetHealthyUsers("all");
+    stat.GetUnfitUsers("USArmy", "male");
+    stat.GetFullStats();
+}
